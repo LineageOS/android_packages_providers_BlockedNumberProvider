8f2c58f5f94d871c7912f92a84072b2a862247e8
Adrian Roos <roosa@google.com>
2025-02-25 15:39:27 +0100

    packages/providers/BlockedNumberProvider: remove deprecated Mockito usages
    Test: m checkbuild
    Flag: TEST_ONLY
    Bug: 310268946
    Change-Id: Idef7d72c855b741fde96b7794b1dc77aaae53d3e

--- tests/src/com/android/providers/blockednumber/BlockedNumberProviderTest.java
+++ tests/src/com/android/providers/blockednumber/BlockedNumberProviderTest.java
@@ -18,9 +18,9 @@ package com.android.providers.blockednumber;
 import static android.os.UserHandle.MIN_SECONDARY_USER_ID;
 import static android.os.UserHandle.USER_SYSTEM;
 
-import static org.mockito.Matchers.anyInt;
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Matchers.eq;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
